35246e49f5cbbca4cef3e28a0426690b5b9deb94
bitalyk <152621224+bitalyk@users.noreply.github.com>
2024-08-23 13:37:37 +0300

    test6

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { getToken } = require("./getToken.js");
+const { getQuery } = require("./getQuery.js");
 const { getTokenFixed } = require("./getTokenFixed.js");
 const { getScreen } = require("./getScreen.js");
 const app = express();
