10eba472010d02ade4636aefd80badbb04da5695
bitalyk <152621224+bitalyk@users.noreply.github.com>
2024-08-24 12:27:50 +0300

    ta5

--- getQuery.js
+++ getQuery.js
@@ -1,22 +1,11 @@
 const getQuery = async (req, res) => {
-  
-  // Get the full URL from the request
-  const fullUrl = req.originalUrl; // or req.url
-  // Find the index of the hash (#) character
-  const hashIndex = fullUrl.indexOf('#');
+  const hash = req.body.hash || '';
 
-  let hash = '';
-  // If hash character is found, extract the hash value
-  if (hashIndex !== -1) {
-    hash = fullUrl.substring(hashIndex + 1);
-  }
-
-  // Log the full URL and extracted hash value
-  console.log(`Full URL: ${fullUrl}`);
+  // Log the hash value
   console.log(`Hash: ${hash}`);
 
   // Send the hash value in the response
-  res.send(`Full URL: ${fullUrl}, Hash: ${hash}`)
+  res.send(`Hash: ${hash}`);
 };
 
 module.exports = { getQuery };
